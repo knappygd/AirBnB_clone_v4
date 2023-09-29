a2814e7728e16c7d187b2a120213607379fffe61
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-09-28 20:41:55 -0700

    Task 03

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    let amenityIDs = {};
+
+    $('input[type="checkbox"]').change(function () {
+        let $checkbox = $(this);
+        let amenityID = $checkbox.data("id");
+
+        if ($checkbox.is(":checked")) {
+            amenityIDs[amenityID] = true;
+        } else {
+            delete amenityIDs[amenityID];
+        }
+
+        let selectedAmenities = Object.keys(amenityIDs).map(function (id) {
+            return amenityIDs[id];
+        });
+
+        $('div.Amenities h4').text(selectedAmenities.join(', '));
+    });
+
+
+    $.ajax({
+        method: 'GET',
+        url: 'http://localhost:5001/api/v1/status/',
+        success: function (data) {
+            if (data.status === 'OK') {
+                $('div#api_status').addClass('available');
+            }
+            else {
+                $('div#api_status').removeClass();
+            }
+        }
+    });
+});
+
+
+fetch('http://0.0.0.0:5001/api/v1/places_search', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({})
+})
+.then(response => response.json())
+.then(data => {
+    const places = data.places;
+
+    const placesSection = $('section.places');
+});

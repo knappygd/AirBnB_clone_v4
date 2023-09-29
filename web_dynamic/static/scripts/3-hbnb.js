1ba7fe65ead798d59f74200f2c4ec49d1c1af7a1
knappygd <emiliodamasco04@gmail.com>
2023-09-29 16:14:50 -0700

    pushed changes

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -19,31 +19,41 @@ $(document).ready(function () {
     });
 
 
-    $.ajax({
-        method: 'GET',
-        url: 'http://localhost:5001/api/v1/status/',
-        success: function (data) {
-            if (data.status === 'OK') {
-                $('div#api_status').addClass('available');
-            }
-            else {
-                $('div#api_status').removeClass();
-            }
+});
+
+$.ajax({
+    method: 'GET',
+    url: 'http://localhost:5001/api/v1/status/',
+    success: function (data) {
+        if (data.status === 'OK') {
+            $('div#api_status').addClass('available');
         }
-    });
+        else {
+            $('div#api_status').removeClass();
+        }
+    }
 });
 
 
 fetch('http://0.0.0.0:5001/api/v1/places_search', {
     method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({})
-})
-.then(response => response.json())
-.then(data => {
-    const places = data.places;
-
-    const placesSection = $('section.places');
+}).then(response => response.json()).then(data => {
+    data.forEach((place) => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <div class="title_box">
+          <h2>${place.name}</h2>
+          <div class="price_by_night">${place.price_by_night}</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${place.max_guest}</div>
+          <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+          <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+        </div>
+        <div class="description">${place.description}</div>
+      `;
+        $('section.places').append(article);
+    });
 });
